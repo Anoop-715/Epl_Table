d13b9a068afa9a922c8a1c09bc4f7cd2deede7fd
Anoop-715 <anoop.a715@gmail.com>
2020-08-17 21:49:29 +0530

    adding src with component structured

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+
+import { setUserSession } from '../UserToken';
+
+
+
+export default function Login() {
+    const [username, setusername] = useState();
+    const [password, setpassword] = useState();
+    const [auth, setAuthe] = useState(false);
+    const [err, setError] = useState();
+
+    const { register, errors, handleSubmit } = useForm();
+
+    function isAuth() {
+        // event.preventDefault();
+        axios.post("/login", {
+            "username": username,
+            "password": password
+        }).then(res => {
+            if (res.data) {
+                setAuthe(res.data);
+                console.log(res);
+            }
+            else {
+                setError("Your credientials are wrong");
+            }
+
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    const onSubmit = data => {
+        console.log(data);
+        isAuth();
+    }
+
+    if (auth) {
+        setUserSession( "AuthenticatedToTrue");
+        // localStorage.setItem("token", "AuthenticatedToTrue");
+        return <Redirect to={{
+            pathname:"/content",
+            state: username
+        }} />
+    }
+    else {
+
+        return (
+
+            <Form className="alert alert-dark  position-absolute" style={{ marginLeft: "40%", marginTop: "10%" }} onSubmit={handleSubmit(onSubmit)}>
+                {!auth ? <p>{err}</p> : null}
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter email" name="username" onChange={e => setusername(e.target.value)}
+                        ref={register({ required: true })} />
+                    {errors.username && <p style={{ color: "red" }}>Your input is Required</p>}
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name="password" onChange={e => setpassword(e.target.value)}
+                        ref={register({ required: true })} />
+                    {errors.password && <p style={{ color: "red" }}>Enter your password here.</p>}
+                </Form.Group>
+                <Button variant="primary" type="submit" >
+                    Submit
+             </Button>
+            </Form>
+        )
+    }
+
+}
